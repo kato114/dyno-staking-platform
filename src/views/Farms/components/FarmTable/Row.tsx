dd2e7c552b86ee3dfd084cce339e18d23c5a7798
Web, Blockchain, Fintech <wwit.llt@gmail.com>
2022-11-28 02:05:12 +0900

    Fix days to months

--- src/views/Farms/components/FarmTable/Row.tsx
+++ src/views/Farms/components/FarmTable/Row.tsx
@@ -129,7 +129,7 @@ const Row: React.FunctionComponent<React.PropsWithChildren<RowPropsWithLoading>>
                   <td key={key}>
                     <CellInner>
                       <CellLayout label={t('Vesting')}>
-                        {isNaN(props.vesting.vesting) ? 'None' : props.vesting.vesting / 3600 / 24 + ' Days'}
+                        {isNaN(props.vesting.vesting) ? 'None' : Math.ceil(props.vesting.vesting / 2678400) + ' Months'}
                       </CellLayout>
                     </CellInner>
                   </td>
